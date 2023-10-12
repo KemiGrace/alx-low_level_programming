c3ee82aaf7b1fe16b899abf4ea56ace8a55515d1
KemiGrace <gracehappy545@gmail.com>
2023-10-11 20:01:28 -0700

    0. Poppy Define a new type struct dog with the following elements:

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - dog structure
+ * @name: name of dog
+ * @age: dog's age
+ * @owner: name of owner
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
