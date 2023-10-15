a524079afb23260bb5b633010c37409eb50b648b
KemiGrace <gracehappy545@gmail.com>
2023-10-14 18:41:15 -0700

    File name

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - prints the name of the file we  compiled from, then a new line
+ *
+ * Return: 0 on sucess
+ */
+int main(void)
+{
+	printf("%s\n", __FILE__);
+
+	return (0);
+}
